6dbdb4c7208ca4141bfeef98a05847827f5c7130
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-10-09 13:59:04 +0100

    fix: Fix class in header

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,5 +1,5 @@
 import styles from "./Header.module.css";
 
 export default function Header({ applyStyle, children }) {
-  return <header className={` ${applyStyle && styles.header}`}>{children}</header>;
+  return <header className={` ${applyStyle ? styles.header : ""}`}>{children}</header>;
 }
