76bb1132aa476892c8fee06c2d88baf69f29660e
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-10-09 19:44:28 +0100

    feat: Add messanger chat bot

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,22 @@ import Main from "./main";
 import Layout from "../components/layout/Layout";
 import Contact from "../components/contact/Contact";
 import ContactForm from "../components/contact-form/ContactForm";
+import { createClient } from "contentful";
 import Footer from "../components/footer/Footer";
 import Hero from "../components/hero/Hero";
 import MetaData from "../components/meta/MetaData";
 import Header from "../components/header/Header";
 import Nav from "../components/navigation/Nav";
+import Messanger from "../components/messanger-chat/Messanger";
+
+// export async function getStaticProps() {
+//   const client = createClient({
+//     space: process.env.CONTENTFUL_SPACE_ID,
+//     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+//   });
+
+//   client;
+// }
 
 export default function Home() {
   return (
@@ -23,6 +34,7 @@ export default function Home() {
         <ContactForm />
       </Layout>
       <Footer />
+      <Messanger />
     </>
   );
 }
