336d1c15c58cc2badf1367c5082422dab96f29a1
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-10-27 12:56:43 +0100

    feat: Add language filter

--- pages/blog.js
+++ pages/blog.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import MetaData from "../components/meta/MetaData";
@@ -9,6 +10,8 @@ import emailIcon from "../public/images/email-icon.png";
 import facebookIcon from "../public/images/fb-icon.png";
 import whatsappIcon from "../public/images/whatsapp.svg";
 import linkedInIcon from "../public/images/linkedin-icon.png";
+import polishFlag from "../public/images/polish-flag.svg";
+import ukFlag from "../public/images/uk-flag.svg";
 import styles from "../styles/blog/Blog.module.css";
 import { createClient } from "contentful";
 
@@ -29,6 +32,11 @@ export async function getStaticProps() {
 }
 
 export default function Blog({ posts }) {
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  function handleLanguage(condition) {
+    const postsInEnglish = posts.filter(post => post.fields.isEnglish === condition);
+    setFilteredPosts(postsInEnglish);
+  }
 
   return (
     <>
@@ -41,10 +49,32 @@ export default function Blog({ posts }) {
       </Header>
       <h1 className="heading">Notary Blog</h1>
       {posts.length === 0 && <h3>There are no posts</h3>}
+
+      {/* Filter POL/ENG */}
+      <div className={styles.language}>
+        <h3>Choose language</h3>
+        <div className={styles["language-options"]}>
+          <Image
+            onClick={() => handleLanguage(false)}
+            width={50}
+            height={50}
+            src={polishFlag}
+            alt="Polish flag"
+          />
+          <Image
+            onClick={() => handleLanguage(true)}
+            width={50}
+            height={50}
+            src={ukFlag}
+            alt="UK flag"
+          />
+        </div>
+      </div>
+
       <div className={`${styles.container} padding`}>
-        {posts.map(post => (
-          <BlogPost key={post.sys.id} post={post} />
-        ))}
+        {filteredPosts.map(post => {
+          return <BlogPost key={post.sys.id} post={post} />;
+        })}
       </div>
       <footer className={styles.footer}>
         <ul className={styles["footer__contact"]}>
